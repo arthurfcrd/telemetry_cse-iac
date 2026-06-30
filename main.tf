9b67a578d44bdf366797e62620cb5285940b34a4
Arthur Foucard <164541563+arthurfcrd@users.noreply.github.com>
2026-06-30 14:55:12 +0000

    fix name

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_storage_container" "newcontainer2" {
 
 # Create MySQL Server 
 resource "azurerm_mysql_flexible_server" "serverformation1" { 
-name    =  "sqlserver" # add your name to make it unique. Can only consist of lowercase letters and numbers, and must be between 3 and 24 characters long.
+name    =  "sqlserverarthur" # add your name to make it unique. Can only consist of lowercase letters and numbers, and must be between 3 and 24 characters long.
 
 location    =  var.location 
 resource_group_name  =  var.rg_name  
